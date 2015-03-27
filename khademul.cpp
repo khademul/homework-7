b127df14a6aeb2d97739d425d55e3541ebf071be
khademul <khademul144@gmail.com>
2015-03-27 17:09:30 -0500

    khademul.cpp

--- khademul.cpp
+++ khademul.cpp
@@ -21,3 +21,19 @@ void printLog(const char *strLog) {
 	printf("%s\n", strLog);
 	fprintf(fpLog, "%s\r\n", strLog);
 }
+int isIntegerString(char *str) {
+	for(unsigned int i=0; i<strlen(str); i++) {
+		if((str[i]>='0' && str[i]<='9') || str[i]=='-' || str[i]=='+') {
+		} else {
+			return 0;
+		}
+	}
+	return 1;
+}
+int atoi_h(char *str) {
+	if(!isIntegerString(str)) {
+		printLog("Error: Integer Expected");
+		exit(1);
+	}
+	return atoi(str);
+}
