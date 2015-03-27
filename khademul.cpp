a4ece9874529001e40fda124848b08793b567667
khademul <khademul144@gmail.com>
2015-03-27 17:05:36 -0500

    main source file

---
+++ khademul.cpp
@@ -0,0 +1,17 @@
+/* Homework 7
+CIVL 8903
+Khademul Haque
+URL:https://github.com/khademul/homework-7
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+#include <stdlib.h>
+#include <fstream>
+
+#include "earthquake.h"
+#include "station.h"
+
+using namespace std;
+
